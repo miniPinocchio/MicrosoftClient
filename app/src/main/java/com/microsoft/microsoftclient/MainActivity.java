a9501775391586cbf6fca0759afac4f06564beb3
wangyingying <61572887@qq.com>
2018-10-31 11:21:13 +0800

    修改首页，添加样式和颜色，删除没用的资源文件ic_launcher_foregroud，和mipmap-anydpi-v26文件夹

--- app/src/main/java/com/microsoft/microsoftclient/MainActivity.java
+++ app/src/main/java/com/microsoft/microsoftclient/MainActivity.java
@@ -1,16 +1,40 @@
 package com.microsoft.microsoftclient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.microsoft.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author huiliu
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.iv_dy_tsk)
+    ImageView ivDyTsk;
+    @BindView(R.id.iv_ks_tsk)
+    ImageView ivKsTsk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initTopBar(this, getResources().getString(R.string.dy_ks));
+    }
+
+    @OnClick({R.id.iv_dy_tsk, R.id.iv_ks_tsk})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_dy_tsk://抖音投票点赞任务
+                break;
+            case R.id.iv_ks_tsk://快手投票点赞任务
+                break;
+        }
     }
 }
